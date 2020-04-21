d90de31b27e165428ce3deb6ce648a9639d9f718
sunlanchang <sunlanchang@outlook.com>
2020-04-21 21:56:59 +0800

    update README

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ python BERT_NER.py \
     --predict_batch_size=8 \
     --data_dir=data/ \
     --vocab_file=vocab.txt \
-    --bert_config_file=cased_L-12_H-768_A-12/bert_config.json \
-    --init_checkpoint=cased_L-12_H-768_A-12/bert_model.ckpt \
+    --bert_config_file=chinese_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=chinese_L-12_H-768_A-12/bert_model.ckpt \
     --max_seq_length=128   \
     --learning_rate=2e-5   \
     --num_train_epochs=1.0 \
