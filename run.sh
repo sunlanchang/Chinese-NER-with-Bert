6ee065bd4dd95f7e78dec0e7f6cbff739ec87998
sunlanchang <sunlanchang@outlook.com>
2020-04-21 23:02:32 +0800

    update git ignore

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ python BERT_NER.py \
     --eval_batch_size=8 \
     --predict_batch_size=8 \
     --data_dir=data/ \
-    --vocab_file=vocab.txt \
+    --vocab_file=chinese_L-12_H-768_A-12/vocab.txt \
     --bert_config_file=chinese_L-12_H-768_A-12/bert_config.json \
     --init_checkpoint=chinese_L-12_H-768_A-12/bert_model.ckpt \
     --max_seq_length=128   \
